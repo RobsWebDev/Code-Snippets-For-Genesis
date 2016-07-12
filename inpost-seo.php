b1635591a863680cb4f00e25a71d185e45ebdc69
Rob McDonald <rob@robmcdonald.net>
2016-07-12 15:23:21 -0400

    Create inpost-seo.php
    Code to remove the Genesis In-Post SEO Settings

---
+++ inpost-seo.php
@@ -0,0 +1,4 @@
+<?php
+//* Do NOT include the opening php tag shown above. Copy the code shown below.
+//* Remove Genesis in-post SEO Settings
+remove_action( 'admin_menu', 'genesis_add_inpost_seo_box' );
