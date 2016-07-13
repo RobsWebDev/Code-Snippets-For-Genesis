52ef90b6ce555549a09be7b07fce51a3eb663847
Rob McDonald <rob@robmcdonald.net>
2016-07-13 15:05:29 -0400

    Create register-widget-area-2.php
    register the widget area in a front-page.php file.

---
+++ register-widget-area-2.php
@@ -0,0 +1,4 @@
+<?php
+//* Do NOT include the opening php tag shown above. Copy the code shown below.
+//* Hook after post widget area after post content
+genesis_widget_area( 'after-post', array( 'before' => '<div class="after-post widget-area">', 'after' => '</div>',
