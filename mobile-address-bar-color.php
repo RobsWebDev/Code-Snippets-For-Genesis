0349861a624fd1e5de45cb82ba6751ae94b696d8
Rob McDonald <rob@robmcdonald.net>
2017-07-18 19:55:55 -0400

    Change function name
    Change function name

--- mobile-address-bar-color.php
+++ mobile-address-bar-color.php
@@ -2,7 +2,7 @@
 /**
  * Change Color of Mobile Address Bar
  */
-function be_mobile_address_bar_color() {
+function rm_mobile_address_bar_color() {
   echo '<meta name="theme-color" content="#000000â€¬" />';
 }
-add_action( 'wp_head', 'be_mobile_address_bar_color' );
+add_action( 'wp_head', 'rm_mobile_address_bar_color' );
